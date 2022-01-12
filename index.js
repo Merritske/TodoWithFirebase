8db917011800f7bdd536ee135a01ed8d4ab2f89d
merritske <merritske@gmail.com>
2022-01-12 14:49:40 +0100

    update security

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
+// let input = document.querySelector(".new-todo-input")
+// const [todoN] = document.querySelector(".input")
+// let list = []
+
+// let count =0
+
+// input.addEventListener("submit", inputfunc)
+
+// function inputfunc(e){
+//     e.preventDefault()
+//  let obj = {
+// todoN : todoN.value
+// }
+// list.push(obj)
+
+
+// let inputL = document.querySelector(".todo-items-wrapper")
+// inputL.innerHTML += `<div class="todo-items">
+// <div class="check">
+//     <div class="check-mark checked">
+//         <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>
+//     </div>
+// </div>
+// <div class="todo-item">
+//     <p>${obj.todoN}</p>
+// </div>
+// </div>`
+
+
+// count ++
+
+
+// todoN.value =""
+
+// }
+
+
 let input = document.querySelector(".new-todo-input")
 const [todoN] = document.querySelector(".input")
+let all = document.querySelector(".all")
 let list = []
 
 let count =0
@@ -7,31 +45,47 @@ let count =0
 input.addEventListener("submit", inputfunc)
 
 function inputfunc(e){
-    e.preventDefault()
- let obj = {
+e.preventDefault()
+let obj = {
 todoN : todoN.value
 }
 list.push(obj)
 
-
 let inputL = document.querySelector(".todo-items-wrapper")
-inputL.innerHTML += `<div class="todo-items">
+inputL.innerHTML += `<div class="todo-items"><div class="todoItems">
 <div class="check">
-    <div class="check-mark checked">
-        <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>
-    </div>
+<div class="check-mark checked">
+ <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>
+ </div>
 </div>
 <div class="todo-item">
-    <p>${obj.todoN}</p>
-</div>
-</div>`
-
+<p>${obj.todoN}</p>
+</div></div> <button onclick="remove(this)">X</button></div>`
 
 count ++
-
+all.innerHTML = `${count} totaal`
 
 todoN.value =""
 
 }
+//remove
+function remove(getEl){
+ getEl.parentElement.remove()
+let itemsLeft = document.querySelector(".itemsleft")
+ count --
+ itemsLeft.innerHTML = `${count} items left`
+}
+//checkmark laten zien als je erop klikt en active zetten
+let checkmark = document.querySelector(".check")
+checkmark.addEventListener("click", check)
+function check (){
+let mark = document.querySelector(".checked")
+mark.style.backgroundColor = "red"
+}
+//als checkmark + delete is completed
+
+//items left = bij remove
+
+
 
 
---
+++ test/app.js
@@ -0,0 +1,24 @@
+let input = document.querySelector(".input")
+let [uitspraak] = document.querySelector(".input-data")
+let list = []
+let count = 0
+input.addEventListener("submit", inputFunc)
+function inputFunc (e){
+e.preventDefault()
+let obj ={
+uitspraak : uitspraak.value
+}
+
+let listIn = document.querySelector(".lijst")
+listIn.innerHTML += `<div class="list" ">
+<div class="count">
+${count}
+</div>
+<div class="quote">
+${obj.uitspraak}
+</div></div>
+`
+list.push(obj)
+
+count ++
+}
