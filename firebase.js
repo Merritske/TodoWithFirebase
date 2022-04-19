43dbdf4d8cd7c69b4dc224180536659d50ac2e44
merritske <merritske@gmail.com>
2022-04-19 22:40:07 +0200

    aanpassing firebase ok

---
+++ firebase.js
@@ -0,0 +1,20 @@
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdA-lwasFmQlvPYGRa3jLWsVGCM1J20ZE",
+  authDomain: "mytodolist-59b8a.firebaseapp.com",
+  projectId: "mytodolist-59b8a",
+  storageBucket: "mytodolist-59b8a.appspot.com",
+  messagingSenderId: "863281685464",
+  appId: "1:863281685464:web:62f71046406ed5574c08ad"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+  var db = firebase.firestore();
+
